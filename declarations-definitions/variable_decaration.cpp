a8170235b781047f6fb0556dcecb8e627da911bf
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-06-17 23:31:06 +0000

    vaiable declaration

---
+++ declarations-definitions/variable_decaration.cpp
@@ -0,0 +1,6 @@
+#include <vector>
+
+int a;                       // no initializer
+double d = 7;                // initializer using the = syntax
+vector<int> vi(10);          // initializerusing the () syntax
+vector<int> vi2{1, 2, 3, 4}; // initializer using the {} synax
